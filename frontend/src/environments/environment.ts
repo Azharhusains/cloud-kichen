1eddf467c2296fd124cb0c486b692f63f4aa11f3
Azharhusains <azharshekh7860@gmail.com>
2026-04-11 20:30:25 +0530

    m

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: false, 
   apiUrl: 'http://localhost:5000/api',
   socketUrl: 'http://localhost:5000',
   clientUrl: 'http://localhost:4200'
